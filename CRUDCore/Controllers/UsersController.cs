4f7dd2237cfd1b1f777d0d592768c501063714d7
novakvova <novakvova@gmail.com>
2019-06-03 18:09:37 +0300

    Get static list users

---
+++ CRUDCore/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDCore.DAL.Entities;
+using CRUDCore.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly EFContext _context;
+        public UsersController(EFContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<UserItemViewModel> GetUsers()
+        {
+            var model = new List<UserItemViewModel>
+            {
+                new UserItemViewModel
+                {
+                    Id=1, Email="jon@gg.ss",
+                    Roles = new List<RoleItemViewModel>
+                    {
+                        new RoleItemViewModel { Id=2, Name="Admin"}
+                    }
+                },
+                new UserItemViewModel
+                {
+                    Id=2, Email="bombelyk@gg.ss",
+                    Roles = new List<RoleItemViewModel>
+                    {
+                        new RoleItemViewModel { Id=2, Name="Admin"},
+                        new RoleItemViewModel { Id=3, Name="Meson"}
+                    }
+                }
+            };
+            return model;
+        }
+    }
+}
